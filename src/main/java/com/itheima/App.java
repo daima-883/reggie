3ada99134b184ac136c4c333a0c208e88b434353
daima-883 <2563663078@qq.com>
2022-11-06 21:21:47 +0800

    登录检查拦截器的编写，测试阶段，但未开发

--- src/main/java/com/itheima/App.java
+++ src/main/java/com/itheima/App.java
@@ -3,7 +3,10 @@ package com.itheima;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @Slf4j//lombok提供
+@ServletComponentScan
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
