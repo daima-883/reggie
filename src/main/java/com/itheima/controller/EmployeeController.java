c7e0912d7358cdc04ada8f57ef4234114d486c5a
daima-883 <2563663078@qq.com>
2022-11-06 11:37:53 +0800

    加入R通用类，删除EmployeeController中无效导入

--- src/main/java/com/itheima/controller/EmployeeController.java
+++ src/main/java/com/itheima/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.itheima.controller;
 import com.itheima.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
