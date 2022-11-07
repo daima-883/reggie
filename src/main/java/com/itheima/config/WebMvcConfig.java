a09d1bd14687b1cad16074c778a9f13505d51692
daima-883 <2563663078@qq.com>
2022-11-07 21:16:17 +0800

    代码修复完成，消息转换器配置完成

--- src/main/java/com/itheima/config/WebMvcConfig.java
+++ src/main/java/com/itheima/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.itheima.config;
 
+import com.itheima.Common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -13,5 +18,16 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("开始加载静态资源。。。。");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    //扩展mvc框架的消息转换器
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将JAVA对象转为JSON
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架集合当中,设置为0，把我们的自定义设置优先使用，否则就用的不是我们自己写的，就等于白写
+        converters.add(0,messageConverter);
     }
 }
