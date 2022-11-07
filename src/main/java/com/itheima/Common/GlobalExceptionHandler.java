93ab06c806e89cae5262fcbcb194159e9ad3b978
daima-883 <2563663078@qq.com>
2022-11-07 16:48:59 +0800

    全局处理架子搭好，功能暂时没做

---
+++ src/main/java/com/itheima/Common/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.itheima.Common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+//全局异常处理类
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.info(ex.getMessage());
+        return R.error("创建失败");
+    }
+}
