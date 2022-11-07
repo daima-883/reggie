1ba3077cabea269d9cf1c28ab0b6f9240eae8152
daima-883 <2563663078@qq.com>
2022-11-07 16:57:37 +0800

    异常处理写完，测试能通过

--- src/main/java/com/itheima/Common/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/Common/GlobalExceptionHandler.java
@@ -17,6 +17,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
         log.info(ex.getMessage());
-        return R.error("创建失败");
+
+        if (ex.getMessage().contains("Duplicate entry")){
+            String[] s = ex.getMessage().split(" ");
+            String msg = s[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
     }
 }
