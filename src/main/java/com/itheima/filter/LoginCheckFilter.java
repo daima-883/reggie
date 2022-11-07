6981b1f1a3bc5a365d93ba72ef609a67e5771331
daima-883 <2563663078@qq.com>
2022-11-07 10:05:43 +0800

    登录拦截器功能完成，同时修改了request.js的超时时间

--- src/main/java/com/itheima/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/filter/LoginCheckFilter.java
@@ -1,6 +1,9 @@
 package com.itheima.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.itheima.Common.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -10,11 +13,46 @@ import java.io.IOException;
 @Slf4j
 @WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
 public class LoginCheckFilter implements Filter {
+    //定义路径匹配变量，专门用于路径匹配，路径匹配器，支持通配符
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        log.info("拦截到请求：{}",request.getRequestURI());
-        filterChain.doFilter(request,response);
+
+        //获取本次请求的URI
+        String requestURI = request.getRequestURI();
+        //定义放行的请求路径
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "front/**"
+        };
+        //不需要处理则直接放行
+        if (check(urls,requestURI)){
+            log.info("本次请求{}不需要验证，直接放行",requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //需要处理，判断是否登录，登录状态则直接放行
+        if (request.getSession().getAttribute("employee") != null){
+            log.info("用户id为：{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //需要处理，不是登录状态，则通过输出流的方式向客户端页面响应数据
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+    //路径匹配，检查请求路径是否需要放行，请求路径是定义放行的路径则返回true,反之返回false
+    public boolean check(String[] urls,String requestURI){
+        for (String url : urls){
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match){
+                return true;
+            }
+        }
+        return false;
     }
 }
