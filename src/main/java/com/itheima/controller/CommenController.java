ec80394daa4519d6d8e571709605a050b680930c
daima-883 <2563663078@qq.com>
2022-11-24 20:19:04 +0800

    文件下载功能代码实现

--- src/main/java/com/itheima/controller/CommenController.java
+++ src/main/java/com/itheima/controller/CommenController.java
@@ -3,12 +3,18 @@ package com.itheima.controller;
 import com.itheima.Common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -47,5 +53,30 @@ public class CommenController {
             e.printStackTrace();
         }
         return R.success(fileName);
+    }
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        //输入流，通过输入流读取文件内容
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(path + name));
+            //设置文件返回格式
+            response.setContentType("image/jpeg");
+            //使用HTTP的输出流进行数据传输给浏览器
+            ServletOutputStream outputStream = response.getOutputStream();
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes))!=-1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
