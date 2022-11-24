eea7fb302d5fbed9e641ad874da1720aafac5a74
daima-883 <2563663078@qq.com>
2022-11-24 20:20:39 +0800

    文件下载功能代码实现（内容没变化，只是强迫症的删除了import中的无用包）

--- src/main/java/com/itheima/controller/CommenController.java
+++ src/main/java/com/itheima/controller/CommenController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
