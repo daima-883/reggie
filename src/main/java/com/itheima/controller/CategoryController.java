8119a8c88891d96e2017842ab608edd6e5b00882
daima-883 <2563663078@qq.com>
2022-11-24 18:02:19 +0800

    修改分类信息功能完成

--- src/main/java/com/itheima/controller/CategoryController.java
+++ src/main/java/com/itheima/controller/CategoryController.java
@@ -37,5 +37,11 @@ public class CategoryController {
         categoryService.remove(id);
         return R.success("删除成功");
     }
+    @PutMapping
+    public R<String> update(@RequestBody Category category){
+        log.info("修改分类信息：{}",category);
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功");
+    }
 
 }
